bf5da202242d4a92774b3fb70ef79cc58ce95aca
Viacheslav Korbut <minowaman777@gmail.com>
2023-12-03 20:25:14 +0200

    Added getters and setters in Pizza

--- pre-marathone-training/sprint03/src/main/java/task01/Pizza.java
+++ pre-marathone-training/sprint03/src/main/java/task01/Pizza.java
@@ -10,6 +10,46 @@ public class Pizza {
     private Pizza() {
     }
 
+    public String getCheese() {
+        return cheese;
+    }
+
+    public void setCheese(String cheese) {
+        this.cheese = cheese;
+    }
+
+    public String getMeat() {
+        return meat;
+    }
+
+    public void setMeat(String meat) {
+        this.meat = meat;
+    }
+
+    public String getSeafood() {
+        return seafood;
+    }
+
+    public void setSeafood(String seafood) {
+        this.seafood = seafood;
+    }
+
+    public String getVegetable() {
+        return vegetable;
+    }
+
+    public void setVegetable(String vegetable) {
+        this.vegetable = vegetable;
+    }
+
+    public String getMushroom() {
+        return mushroom;
+    }
+
+    public void setMushroom(String mushroom) {
+        this.mushroom = mushroom;
+    }
+
     public static PizzaBuilder base() {
         return new PizzaBuilder();
     }
