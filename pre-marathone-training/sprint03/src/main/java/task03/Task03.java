e8626c776f2f8d99cc8697d527cb6b59d336f8ee
Viacheslav Korbut <minowaman777@gmail.com>
2023-12-05 18:22:29 +0200

    Work with task03 from sprint03 new version

--- pre-marathone-training/sprint03/src/main/java/task03/Task03.java
+++ pre-marathone-training/sprint03/src/main/java/task03/Task03.java
@@ -3,12 +3,14 @@ package task03;
 public class Task03 {
     public static void main(String[] args) {
         addition(5, 5);
+        subtraction(5, 2);
+        multiplication(2, 3);
+        division(10, 2);
     }
 
     public static void execute(int a, int b, Strategy strategy) {
         double result = strategy.doOperation(a, b);
-        String nameStrategy = strategy.getClass().getDeclaredMethods()[0].getName();
-        System.out.println(nameStrategy + ": " + result);
+        System.out.println(result);
     }
 
     public static void addition(int a, int b) {
@@ -16,6 +18,36 @@ public class Task03 {
             @Override
             public double doOperation(int a, int b) {
                 return a + b;
+            }
+        });
+    }
+
+    public static void subtraction(int a, int b) {
+        execute(a, b, new Strategy() {
+            @Override
+            public double doOperation(int a, int b) {
+                return a - b;
+            }
+        });
+    }
+
+    public static void multiplication(int a, int b) {
+        execute(a, b, new Strategy() {
+            @Override
+            public double doOperation(int a, int b) {
+                return a * b;
+            }
+        });
+    }
+
+    public static void division(int a, int b) {
+        execute(a, b, new Strategy() {
+            @Override
+            public double doOperation(int a, int b) {
+                if (b == 0) {
+                    throw new ArithmeticException("It`s not possible to divide by zero.");
+                }
+                return a / b;
             }
         });
     }
