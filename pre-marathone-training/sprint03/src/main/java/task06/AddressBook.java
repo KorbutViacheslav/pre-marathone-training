38b4ea7fb0ac22c275340b2f4ea5534ca0a73f5f
Viacheslav Korbut <minowaman777@gmail.com>
2023-12-07 15:35:23 +0200

    Added inner classes NameAddressPair and Person.

--- pre-marathone-training/sprint03/src/main/java/task06/AddressBook.java
+++ pre-marathone-training/sprint03/src/main/java/task06/AddressBook.java
@@ -1,4 +1,23 @@
 package task06;
 
 public class AddressBook {
+    class NameAddressPair {
+        private Person person;
+        private String address;
+
+        public NameAddressPair(Person person, String address) {
+            this.person = person;
+            this.address = address;
+        }
+    }
+
+    class Person {
+        private String firstName;
+        private String lastName;
+
+        public Person(String firstName, String lastName) {
+            this.firstName = firstName;
+            this.lastName = lastName;
+        }
+    }
 }
