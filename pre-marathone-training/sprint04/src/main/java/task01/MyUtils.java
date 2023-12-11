391e807b982ce7870c16cb5f6d6b7f524b0b4bcc
Viacheslav Korbut <minowaman777@gmail.com>
2023-12-11 15:25:43 +0200

    Completed task01 from sprint04

--- pre-marathone-training/sprint04/src/main/java/task01/MyUtils.java
+++ pre-marathone-training/sprint04/src/main/java/task01/MyUtils.java
@@ -1,13 +1,14 @@
 package task01;
 
+import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 import java.util.stream.Collectors;
 
 public class MyUtils {
     public static void main(String[] args) {
-        //{0967654321=Petro, 0677654321=Petro, 0501234567=Ivan, 0970011223=Stepan, 0631234567=Ivan}
-        Map<String, String> phoneName = new HashMap();
+        Map<String, String> phoneName = new HashMap<>();
         phoneName.put("0967654321", "Petro");
         phoneName.put("0677654321", "Petro");
         phoneName.put("0501234567", "Ivan");
@@ -18,9 +19,25 @@ public class MyUtils {
 
     }
 
-    public static Map<String, String> createNotebook(Map<String, String> map) {
-        Map<String,String> currentMap = new HashMap<>(map);
-        Collectors.groupingBy(map.av)
-        return new HashMap(map);
+    public static Map<String, List<String>> createNotebook(Map<String, String> map) {
+/*        Map<String, List<String>> currentMap = new HashMap<>();
+        for (Map.Entry<String, String> entry : map.entrySet()) {
+            String name = entry.getValue();
+            String phone = entry.getKey();
+            if (!currentMap.containsKey(name)) {
+                List<String> phoneNumber = new ArrayList<>();
+                phoneNumber.add(phone);
+                currentMap.put(name, phoneNumber);
+            } else {
+                currentMap.get(name).add(phone);
+            }
+        }
+        return currentMap;*/
+        return map.entrySet()
+                .stream()
+                .collect(Collectors.groupingBy(
+                        Map.Entry::getValue,
+                        Collectors.mapping(Map.Entry::getKey, Collectors.toList())
+                ));
     }
 }
