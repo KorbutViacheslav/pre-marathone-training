e6b6d303e22653cf367fdcd17ce1ce874409453f
Viacheslav Korbut <minowaman777@gmail.com>
2023-12-16 18:55:30 +0200

    Created method getPredicateFromSet(...)

--- pre-marathone-training/sprint05/src/main/java/task05/MyUtils.java
+++ pre-marathone-training/sprint05/src/main/java/task05/MyUtils.java
@@ -1,4 +1,12 @@
 package task05;
 
+import java.util.Set;
+
 public class MyUtils {
+    public static void main(String[] args) {
+
+    }
+    public static void getPredicateFromSet(Set<Integer> set){
+
+    }
 }
