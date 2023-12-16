5d463bd8daf1fe0ea9d449b0bca068892f9e617e
Viacheslav Korbut <minowaman777@gmail.com>
2023-12-16 16:28:01 +0200

    Fix and added best solution with stream API.

--- pre-marathone-training/sprint05/src/main/java/task03/App.java
+++ pre-marathone-training/sprint05/src/main/java/task03/App.java
@@ -5,16 +5,22 @@ import java.util.List;
 import java.util.function.BinaryOperator;
 
 public class App {
-    private static final BinaryOperator<String> greetingOperator = (param1, param2) -> {
-        return "Hello " + param1 + " " + param2 + "!!!";
-    };
+    private static final BinaryOperator<String> greetingOperator = (param1, param2) -> "Hello " + param1 + " " + param2 + "!!!";
 
-    public static List<String> createGreetings(List<Person> list, BinaryOperator<String> binaryOperator) {
+    /** Rough solution */
+/*    public static List<String> createGreetings(List<Person> list, BinaryOperator<String> binaryOperator) {
         List<String> resultList = new ArrayList<>();
         for (Person p : list) {
             resultList.add(binaryOperator.apply(p.name, p.surname));
         }
         return resultList;
+    }*/
+
+    /**
+     * Best solution
+     */
+    public static List<String> createGreetings(List<Person> list, BinaryOperator<String> binaryOperator) {
+        return list.stream().map(person -> binaryOperator.apply(person.name, person.surname)).toList();
     }
 
     public static void main(String[] args) {
@@ -22,6 +28,6 @@ public class App {
                 new Person("Adam", "Walker"),
                 new Person("John", "Dou"),
                 new Person("Rob", "Williams"));
-        createGreetings(personList,greetingOperator).forEach(System.out::println);
+        createGreetings(personList, greetingOperator).forEach(System.out::println);
     }
 }
