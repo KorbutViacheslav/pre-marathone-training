78552bfdee366946f58ae138da7e7cf3aba1eb49
Viacheslav Korbut <minowaman777@gmail.com>
2023-12-16 20:39:46 +0200

    Implementation getPredicateFromSet

--- pre-marathone-training/sprint05/src/main/java/task05/MyUtils.java
+++ pre-marathone-training/sprint05/src/main/java/task05/MyUtils.java
@@ -1,12 +1,17 @@
 package task05;
 
 import java.util.Set;
+import java.util.function.Predicate;
 
 public class MyUtils {
     public static void main(String[] args) {
 
     }
-    public static void getPredicateFromSet(Set<Integer> set){
-
+    public static Predicate<Integer> getPredicateFromSet(Set<Predicate<Integer>> set){
+        Predicate<Integer> predicate = null;
+        for(Predicate<Integer> pred:set){
+            predicate.and(pred);
+        }
+        return predicate;
     }
 }
