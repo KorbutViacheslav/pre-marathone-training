684df8b8358545fd340cd9d456e7181a3b0b2297
Viacheslav Korbut <minowaman777@gmail.com>
2023-12-16 21:03:36 +0200

    Completed task05 from sprint05

--- pre-marathone-training/sprint05/src/main/java/task05/MyUtils.java
+++ pre-marathone-training/sprint05/src/main/java/task05/MyUtils.java
@@ -9,21 +9,18 @@ public class MyUtils {
         Predicate<Integer> predicate = integer -> integer > 0;
         Predicate<Integer> predicate2 = integer -> integer != 5;
         Predicate<Integer> predicate3 = integer -> integer < 7;
-        Set<Predicate<Integer>> predicateSet = Set.of(predicate,predicate2,predicate3);
+        Set<Predicate<Integer>> predicateSet = Set.of(predicate, predicate2, predicate3);
+
         Set<Integer> integerSet = Set.of(-1, 1, 2, 3, 4, 5, 6, 7);
         integerSet.stream().filter(getPredicateFromSet(predicateSet)).toList().forEach(System.out::println);
+
 
     }
 
     public static Predicate<Integer> getPredicateFromSet(Set<Predicate<Integer>> set) {
-        Predicate<Integer> predicate = new Predicate<Integer>() {
-            @Override
-            public boolean test(Integer integer) {
-                return true;
-            }
-        };
+        Predicate<Integer> predicate = integer -> true;
         for (Predicate<Integer> pred : set) {
-            predicate.and(pred);
+            predicate = predicate.and(pred);
         }
         return predicate;
     }
