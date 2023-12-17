0173dd5e4febf17499345e79fcf2bba7520828ed
Viacheslav Korbut <minowaman777@gmail.com>
2023-12-17 16:26:02 +0200

    Added User class

--- pre-marathone-training/sprint05/src/main/java/task06/MyUtils.java
+++ pre-marathone-training/sprint05/src/main/java/task06/MyUtils.java
@@ -1,4 +1,14 @@
 package task06;
 
+import java.util.List;
+import java.util.function.Predicate;
+
 public class MyUtils {
+    public static void main(String[] args) {
+
+    }
+
+    public static int findMaxByCondition(List<Integer> list, Predicate<Integer> predicate) {
+        return list.stream().filter(predicate).toList().get(0);
+    }
 }
---
+++ pre-marathone-training/sprint05/src/main/java/task06/User.java
@@ -0,0 +1,9 @@
+package task06;
+
+import java.util.function.Predicate;
+
+class User {
+    public static int getFilterdValue(int num, Predicate<Integer> predicate) {
+        return 0;
+    }
+}
