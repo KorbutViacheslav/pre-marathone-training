ad09831f1a771d8e72eeaf7c6c90dafdd40ed644
Viacheslav Korbut <minowaman777@gmail.com>
2023-12-18 14:58:26 +0200

    Completed task06 from sprint 05 but not understanding task

--- pre-marathone-training/sprint05/src/main/java/task06/MyUtils.java
+++ pre-marathone-training/sprint05/src/main/java/task06/MyUtils.java
@@ -1,14 +1,23 @@
 package task06;
 
 import java.util.List;
+import java.util.function.BiFunction;
 import java.util.function.Predicate;
 
 public class MyUtils {
     public static void main(String[] args) {
+        List<Integer> integerList = List.of(1, 2, 3, 4, 5, 6, 0, -1, -3);
+        User user = new User(integerList);
+
+        Predicate<Integer> pred = us -> us > 3;
+        BiFunction<List<Integer>, Predicate<Integer>, Integer> biFunction = (list, predicate) -> findMaxByCondition(list, predicate);
+        System.out.println(User.getMaxValueByCondition(biFunction, pred));
+        Predicate<Integer> lastPred = s -> s == 6;
+        System.out.println(User.getFilterdValue(lastPred));
 
     }
 
     public static int findMaxByCondition(List<Integer> list, Predicate<Integer> predicate) {
-        return list.stream().filter(predicate).toList().get(0);
+        return list.stream().filter(predicate).max(Integer::compareTo).get();
     }
 }
--- pre-marathone-training/sprint05/src/main/java/task06/User.java
+++ pre-marathone-training/sprint05/src/main/java/task06/User.java
@@ -1,9 +1,21 @@
 package task06;
 
+import java.util.List;
+import java.util.function.BiFunction;
 import java.util.function.Predicate;
 
 class User {
-    public static int getFilterdValue(int num, Predicate<Integer> predicate) {
-        return 0;
+    public static List<Integer> values;
+
+    public User(List<Integer> values) {
+        User.values = values;
+    }
+
+    public static int getFilterdValue(Predicate<Integer> predicate) {
+        return getMaxValueByCondition(MyUtils::findMaxByCondition, predicate);
+    }
+
+    public static int getMaxValueByCondition(BiFunction<List<Integer>, Predicate<Integer>, Integer> num, Predicate<Integer> predicate) {
+        return num.apply(values, predicate);
     }
 }
