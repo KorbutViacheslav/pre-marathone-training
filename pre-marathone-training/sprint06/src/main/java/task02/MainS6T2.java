fb3959a64c5d47c28fea666d79abef8e6f3a5703
Viacheslav Korbut <minowaman777@gmail.com>
2023-12-19 13:02:12 +0200

    fix

--- pre-marathone-training/sprint06/src/main/java/task02/MainS6T2.java
+++ pre-marathone-training/sprint06/src/main/java/task02/MainS6T2.java
@@ -8,15 +8,10 @@ import java.time.temporal.ChronoUnit;
 
 public class MainS6T2 {
     public static void main(String[] args) {
-
         System.out.println(getDateAfterToday(1, 10, 12));
     }
 
     public static String getDateAfterToday(int y, int m, int d) {
-        Period period = Period.of(y, m, d);
-        LocalDate localDateNow = LocalDate.now();
-
-        LocalDate result = localDateNow.plus(period);
-        return result.format(DateTimeFormatter.ISO_LOCAL_DATE);
+        return LocalDate.now().plus(Period.of(y, m, d)).format(DateTimeFormatter.ISO_LOCAL_DATE);
     }
 }
