46cf1271cd435bbbd43c051086dd84be05691d51
Viacheslav Korbut <minowaman777@gmail.com>
2023-12-19 10:53:44 +0200

    Completed task02 from sprint06

--- pre-marathone-training/sprint06/src/main/java/task02/MainS6T2.java
+++ pre-marathone-training/sprint06/src/main/java/task02/MainS6T2.java
@@ -9,23 +9,14 @@ import java.time.temporal.ChronoUnit;
 public class MainS6T2 {
     public static void main(String[] args) {
 
-        System.out.println(getDateAfterToday(2023, 12, 19));
+        System.out.println(getDateAfterToday(1, 10, 12));
     }
 
     public static String getDateAfterToday(int y, int m, int d) {
-        LocalDate localDate = LocalDate.of(y, m, d);
+        Period period = Period.of(y, m, d);
         LocalDate localDateNow = LocalDate.now();
-        Period dif;
-        if(ChronoUnit.DAYS.between(localDateNow,localDate)==0){
-            return LocalDate.of(0,0,0).format(DateTimeFormatter.ISO_LOCAL_DATE);
-        }
-        if (ChronoUnit.DAYS.between(localDateNow,localDate)>0) {
-            dif = Period.between(localDateNow, localDate);
-        } else {
-            dif = Period.between(localDate, localDateNow);
-        }
 
-        LocalDate result = LocalDate.of(dif.getYears(), dif.getMonths(), dif.getDays());
+        LocalDate result = localDateNow.plus(period);
         return result.format(DateTimeFormatter.ISO_LOCAL_DATE);
     }
 }
