c92abc3ab6c98502aaa5416832c8843466a449a2
Viacheslav Korbut <minowaman777@gmail.com>
2023-12-19 08:33:28 +0200

    Found a solution to the problem

--- pre-marathone-training/sprint06/src/main/java/task02/MainS6T2.java
+++ pre-marathone-training/sprint06/src/main/java/task02/MainS6T2.java
@@ -2,18 +2,30 @@ package task02;
 
 import java.time.LocalDate;
 import java.time.LocalDateTime;
+import java.time.Period;
 import java.time.format.DateTimeFormatter;
+import java.time.temporal.ChronoUnit;
 
 public class MainS6T2 {
     public static void main(String[] args) {
-        LocalDateTime s = LocalDateTime.now();
-        System.out.println(s.format(DateTimeFormatter.ISO_DATE));
 
+        System.out.println(getDateAfterToday(2023, 12, 19));
     }
 
-    public static LocalDate getDateAfterToday(int y, int m, int d) {
-        //LocalDate localDate = LocalDate.now() + LocalDate.of(y, m, d);
-        //localDate.format(DateTimeFormatter.ISO_DATE);
-        return LocalDate.now();
+    public static String getDateAfterToday(int y, int m, int d) {
+        LocalDate localDate = LocalDate.of(y, m, d);
+        LocalDate localDateNow = LocalDate.now();
+        Period dif;
+        if(ChronoUnit.DAYS.between(localDateNow,localDate)==0){
+            return LocalDate.of(0,0,0).format(DateTimeFormatter.ISO_LOCAL_DATE);
+        }
+        if (ChronoUnit.DAYS.between(localDateNow,localDate)>0) {
+            dif = Period.between(localDateNow, localDate);
+        } else {
+            dif = Period.between(localDate, localDateNow);
+        }
+
+        LocalDate result = LocalDate.of(dif.getYears(), dif.getMonths(), dif.getDays());
+        return result.format(DateTimeFormatter.ISO_LOCAL_DATE);
     }
 }
