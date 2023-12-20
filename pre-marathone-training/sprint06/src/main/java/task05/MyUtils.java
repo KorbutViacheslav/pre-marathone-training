d7dfe646392400c0ab839699a8b2fc6a2c9c284d
Viacheslav Korbut <minowaman777@gmail.com>
2023-12-20 20:05:16 +0200

    Completed task05 from sprint06

--- pre-marathone-training/sprint06/src/main/java/task05/MyUtils.java
+++ pre-marathone-training/sprint06/src/main/java/task05/MyUtils.java
@@ -3,22 +3,32 @@ package task05;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Map;
+import java.util.Objects;
+import java.util.stream.Collectors;
 import java.util.stream.Stream;
 
 public class MyUtils {
-    public static void main(String[] args) {
-
-
+    public static Stream<String> nameList(Map<String, Stream<String>> map) {
+        return map.values().stream()
+                .flatMap(i -> i)
+                .filter(s -> s != null && !s.isBlank())
+                .map(item -> item.replaceAll("\\s+", ""))
+                .map(item -> item.substring(0, 1).toUpperCase() + item.substring(1).toLowerCase())
+                .distinct()
+                .sorted();
     }
 
-    public static Stream<String> nameList(Map<String, Stream<String>> map) {
-        Stream<String> result ;
-        List<String> l =new ArrayList<>();
-        for (Map.Entry<String, Stream<String>> stream : map.entrySet()) {
+    public static void main(String[] args) {
+        // Приклад використання
+        Map<String, Stream<String>> projectParticipants = Map.of(
+                "Desktop", Stream.of(" iVan", "PeTro ", " Ira "),
+                "Web", Stream.of("STepan", "ira ", " Andriy ", "an na"),
+                "Spring", Stream.of("Ivan", "Anna")
+        );
 
-            l.add(List.of(stream.getValue().toList()).toString());
-        }
-        result = l.stream();
-        return result;
+        List<String> sortedParticipants = nameList(projectParticipants)
+                .collect(Collectors.toList());
+
+        System.out.println(sortedParticipants);
     }
 }
