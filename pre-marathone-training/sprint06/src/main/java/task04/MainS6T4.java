e17087cd8f6da84dc458b4009e3763f108bf3ca9
Viacheslav Korbut <minowaman777@gmail.com>
2023-12-20 19:02:50 +0200

    gpt solution

--- pre-marathone-training/sprint06/src/main/java/task04/MainS6T4.java
+++ pre-marathone-training/sprint06/src/main/java/task04/MainS6T4.java
@@ -2,6 +2,7 @@ package task04;
 
 import java.nio.file.Files;
 import java.nio.file.Paths;
+import java.util.regex.Pattern;
 
 public class MainS6T4 {
     public static void main(String[] args) {
@@ -9,11 +10,11 @@ public class MainS6T4 {
         // Read from file
         String readText = readFile(filePath);
         System.out.println("Text read from file: " + readText);
-        System.out.println(binaryToText(readText));
+        //System.out.println(binaryToText(readText));
 
     }
 
-    //Method for reading from a file
+    /*//Method for reading from a file
     public static String readFile(String filePath) {
         try {
             byte[] bytes = Files.readAllBytes(Paths.get(filePath));
@@ -33,5 +34,20 @@ public class MainS6T4 {
             text.append((char) charValue);
         }
         return text.toString();
+    }*/
+
+    /**
+     * New best solution from gpt
+     */
+    // Method for reading from a file
+    public static String readFile(String filePath) {
+        try {
+            String binaryText = new String(Files.readAllBytes(Paths.get(filePath)), "UTF-8");
+            return Pattern.compile(".{7}").matcher(binaryText)
+                    .replaceAll(m -> String.valueOf((char) Integer.parseInt(m.group(), 2)));
+        } catch (Exception e) {
+            e.printStackTrace();
+            return null;
+        }
     }
 }
