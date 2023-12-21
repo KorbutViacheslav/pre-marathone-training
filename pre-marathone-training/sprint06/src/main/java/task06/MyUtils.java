0f4a7dffa65adef87d9ceef15e87fcb1fcefdd3b
Viacheslav Korbut <minowaman777@gmail.com>
2023-12-21 15:56:31 +0200

    first etap

--- pre-marathone-training/sprint06/src/main/java/task06/MyUtils.java
+++ pre-marathone-training/sprint06/src/main/java/task06/MyUtils.java
@@ -1,15 +1,25 @@
 package task06;
 
-import java.util.List;
-import java.util.Map;
+import java.util.*;
+import java.util.stream.Collectors;
 import java.util.stream.Stream;
 
 class MyUtils {
     public static void main(String[] args) {
+        List<Stream<String>> givenList = Arrays.asList(
+                Stream.of("093 987 65 43", "(050)1234567", "12-345"),
+                Stream.of("067-21-436-57", "050-2345678", "0939182736", "224-19-28"),
+                Stream.of("(093)-11-22-334", "044 435-62-18", "721-73-45"));
+        Map<String, Stream<String>> result = new MyUtils().phoneNumbers(givenList);
+        result.forEach((key, value) -> {
+            System.out.print(key + " : ");
+            System.out.println(value.collect(Collectors.joining(" , ")));
+        });
 
     }
     public static Map<String, Stream<String>> phoneNumbers(List<Stream<String>> list){
-
-        return null;
+        Map<String, Stream<String>> mapS = new HashMap<>();
+        mapS.put("1",list.stream().flatMap(stringStream -> stringStream));
+        return mapS;
     }
 }
