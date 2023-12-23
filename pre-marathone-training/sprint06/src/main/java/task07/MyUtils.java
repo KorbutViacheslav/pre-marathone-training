4e63d951db85cf7d87b6bf234044eaa7380646dd
Viacheslav Korbut <minowaman777@gmail.com>
2023-12-23 20:11:27 +0200

    main work

--- pre-marathone-training/sprint06/src/main/java/task07/MyUtils.java
+++ pre-marathone-training/sprint06/src/main/java/task07/MyUtils.java
@@ -1,13 +1,17 @@
 package task07;
 
+import java.util.List;
+import java.util.stream.Collectors;
 import java.util.stream.Stream;
 
 public class MyUtils {
     public static void main(String[] args) {
+        List<Integer> result = duplicateElements(Stream.of(3, 2, 1, 1, 12, 3, 8, 2, 4, 2)).toList();
 
+        System.out.println(result);
     }
 
-    public static Stream duplicateElements(Stream stream) {
+    public static Stream<Integer> duplicateElements(Stream<Integer> stream) {
         return null;
     }
 }
