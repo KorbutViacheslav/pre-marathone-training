02cf05750d75325fb0d6d44d1d2ee0f41e1c640c
Viacheslav Korbut <minowaman777@gmail.com>
2023-12-27 14:41:50 +0200

    Added BinaryOperator and 2 opernds, imlements method run

--- pre-marathone-training/sprint07/src/main/java/task01/ParallelCalculator.java
+++ pre-marathone-training/sprint07/src/main/java/task01/ParallelCalculator.java
@@ -1,7 +1,20 @@
 package task01;
 
-public class ParallelCalculator implements Runnable {
-    public void run() {
+import java.util.function.BinaryOperator;
 
+public class ParallelCalculator implements Runnable {
+    BinaryOperator<Integer> binaryOperator = Integer::sum;
+    int operand1;
+    int operand2;
+    double result;
+
+    public ParallelCalculator(BinaryOperator<Integer> binaryOperator, int operand1, int operand2) {
+        this.binaryOperator = binaryOperator;
+        this.operand1 = operand1;
+        this.operand2 = operand2;
+    }
+
+    public void run() {
+        binaryOperator.apply(operand1, operand2);
     }
 }
