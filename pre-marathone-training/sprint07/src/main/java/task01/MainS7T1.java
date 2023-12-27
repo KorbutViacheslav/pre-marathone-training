af7dc239ac673f572a27a1c19e5ba6e1d54fed62
Viacheslav Korbut <minowaman777@gmail.com>
2023-12-27 15:06:08 +0200

    Completed task01 from sprint07

--- pre-marathone-training/sprint07/src/main/java/task01/MainS7T1.java
+++ pre-marathone-training/sprint07/src/main/java/task01/MainS7T1.java
@@ -1,15 +1,21 @@
 package task01;
 
+import java.util.List;
+
 public class MainS7T1 {
     public static void main(String[] args) {
+
+        //Created new ParallelCalculator and set parameters BinaryOperator and 2 operands.
         ParallelCalculator pC1 = new ParallelCalculator(Integer::sum, 6, 2);
         ParallelCalculator pC2 = new ParallelCalculator((o1, o2) -> o1 - o2, 6, 2);
         ParallelCalculator pC3 = new ParallelCalculator((o1, o2) -> o1 * o2, 6, 2);
         ParallelCalculator pC4 = new ParallelCalculator((o1, o2) -> o1 / o2, 6, 2);
-        new Thread(pC1).start();
-        new Thread(pC2).start();
-        new Thread(pC3).start();
-        new Thread(pC4).start();
+
+        //Added all ParallelCalculator to list of Threads and transmit to.
+        List<Thread> threadList = List.of(new Thread(pC1), new Thread(pC2), new Thread(pC3), new Thread(pC4));
+
+        //Called method start from all thread.
+        threadList.forEach(Thread::start);
 
     }
 }
