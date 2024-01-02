53b4d5bbc3ee2f34489012ba2e8539a779aa6a6b
Viacheslav Korbut <minowaman777@gmail.com>
2024-01-02 21:35:41 +0200

    b

--- pre-marathone-training/sprint08/src/main/java/MainS8.java
+++ pre-marathone-training/sprint08/src/main/java/MainS8.java
@@ -2,6 +2,7 @@ import java.sql.SQLException;
 
 public class MainS8 {
     public static void main(String[] args) throws SQLException {
+
         MyUtils myUtils = new MyUtils();
         myUtils.createConnection();
         myUtils.createStatement();
