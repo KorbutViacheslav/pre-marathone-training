08c0e6bc87e11ecebeff2df8322bf2f11370679e
Viacheslav Korbut <minowaman777@gmail.com>
2024-01-02 12:53:25 +0200

    Created some methods in MyUtils

--- pre-marathone-training/sprint08/src/main/java/MyUtils.java
+++ pre-marathone-training/sprint08/src/main/java/MyUtils.java
@@ -1,2 +1,33 @@
+import java.sql.Connection;
+import java.sql.SQLException;
+import java.sql.Statement;
+
 public class MyUtils {
+    public Connection createConnection() throws SQLException {
+        return null;
+    }
+
+    public void closeConnection() throws SQLException {
+
+    }
+
+    public Statement createStatement() throws SQLException {
+        return null;
+    }
+
+    public void closeStatement() throws SQLException {
+
+    }
+
+    public void createSchema(String schemaName) throws SQLException {
+
+    }
+
+    public void dropSchema() throws SQLException {
+
+    }
+
+    public void useSchema() throws SQLException {
+
+    }
 }
