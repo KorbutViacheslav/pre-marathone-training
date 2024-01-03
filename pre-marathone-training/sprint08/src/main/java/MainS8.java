5214342d833c23a55050413f090992df0aae7042
Viacheslav Korbut <minowaman777@gmail.com>
2024-01-03 18:33:34 +0200

    Completed all task

--- pre-marathone-training/sprint08/src/main/java/MainS8.java
+++ pre-marathone-training/sprint08/src/main/java/MainS8.java
@@ -16,23 +16,31 @@ public class MainS8 {
         //myUtils.insertTableRoles("Developer");
 
         myUtils.createTableDirections();
-        //myUtils.insertTableDirections("Java");
+        //myUtils.insertTableDirections("Piton");
 
         myUtils.createTableProjects();
-        //myUtils.insertTableProjects("JavaAPI","Java");
+        //myUtils.insertTableProjects("PitonAPI","Piton");
+        //myUtils.insertTableProjects("JavaEE","Java");
 
         myUtils.createTableEmployee();
-        //myUtils.insertTableEmployee("Max","Developer","JavaAPI");
+        //myUtils.insertTableEmployee("Slava","Developer","JavaEE");
+        //myUtils.insertTableEmployee("Denis","Developer","PitonAPI");
 
         System.out.println(myUtils.getRoleId("Developer")
                 + ", " + myUtils.getDirectionId("Java")
                 + ", " + myUtils.getProjectId("JavaAPI")
                 + ", " + myUtils.getEmployeeId("Max"));
+        System.out.println("-Roles-");
         myUtils.getAllRoles().forEach(System.out::println);
+        System.out.println("-Directions-");
         myUtils.getAllDirection().forEach(System.out::println);
+        System.out.println("-Projects-");
         myUtils.getAllProjects().forEach(System.out::println);
+        System.out.println("-Employees-");
         myUtils.getAllEmployee().forEach(System.out::println);
+        System.out.println("-Developers-");
         myUtils.getAllDevelopers().forEach(System.out::println);
+        System.out.println("-JavaDevelopers-");
         myUtils.getAllJavaDevelopers().forEach(System.out::println);
     }
 }
