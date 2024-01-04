50afccfd39e367e250f66fdebb01b3f55c26e58a
Viacheslav Korbut <minowaman777@gmail.com>
2024-01-04 20:26:25 +0200

    a

--- pre-marathone-training/sprint09/src/main/java/org/example/App.java
+++ pre-marathone-training/sprint09/src/main/java/org/example/App.java
@@ -2,12 +2,9 @@ package org.example;
 
 /**
  * Hello world!
- *
  */
-public class App 
-{
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World from sprint09!" );
+public class App {
+    public static void main(String[] args) {
+        System.out.println("Hello World from sprint09!");
     }
 }
