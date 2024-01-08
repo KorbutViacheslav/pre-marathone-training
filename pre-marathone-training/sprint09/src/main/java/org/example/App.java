d63c157d84c31a46ce45d59b0476b6226a38adf8
Viacheslav Korbut <minowaman777@gmail.com>
2024-01-08 15:30:00 +0200

    URL training

--- pre-marathone-training/sprint09/src/main/java/org/example/App.java
+++ pre-marathone-training/sprint09/src/main/java/org/example/App.java
@@ -1,12 +1,18 @@
 package org.example;
 
+import java.io.IOException;
+import java.net.URL;
+import java.net.URLConnection;
+
 /**
  * Hello world!
  */
 public class App {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
         System.out.println("Hello World from sprint09!");
-        Task task = new Task();
 
+        var url = new URL("file:E:\\JAVA\\projekt\\pre-marathone-training\\pre-marathone-training\\sprint09\\src\\main\\java\\org\\example\\Priority.java");
+        var urlConnection = url.openConnection();
+        System.out.println(new String(urlConnection.getInputStream().readAllBytes()));
     }
 }
