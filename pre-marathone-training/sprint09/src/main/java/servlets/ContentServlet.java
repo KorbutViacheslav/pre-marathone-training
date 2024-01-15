e8d1865f99da1eb23cf32acddc81399c70c66dc5
Viacheslav Korbut <minowaman777@gmail.com>
2024-01-15 15:40:12 +0200

    b

--- pre-marathone-training/sprint09/src/main/java/servlets/ContentServlet.java
+++ pre-marathone-training/sprint09/src/main/java/servlets/ContentServlet.java
@@ -5,6 +5,7 @@ import jakarta.servlet.annotation.WebServlet;
 import jakarta.servlet.http.HttpServlet;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
+import util.JspHelper;
 
 import java.io.IOException;
 
@@ -12,6 +13,6 @@ import java.io.IOException;
 public class ContentServlet extends HttpServlet {
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-        req.getRequestDispatcher("/WEB-INF/jsp/content.jsp").forward(req, resp);
+        req.getRequestDispatcher(JspHelper.getPath("content")).forward(req, resp);
     }
 }
