26f159dae1df49d4402765ee56cf44611e71ae7f
Viacheslav Korbut <minowaman777@gmail.com>
2024-02-06 15:58:12 +0200

    Added create and delete servlets, refactoring

---
+++ pre-marathone-training/sprint09/src/main/java/servlets/CreateTaskServlet.java
@@ -0,0 +1,45 @@
+package servlets;
+
+import model.Priority;
+import model.Task;
+import repository.TaskRepo;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+import java.io.IOException;
+
+@WebServlet(value = "/create-task")
+public class CreateTaskServlet extends HttpServlet {
+
+    private TaskRepo taskRepository;
+
+    @Override
+    public void init() {
+        taskRepository = new TaskRepo();
+    }
+
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        request.getRequestDispatcher("/WEB-INF/pages/create-task.jsp").forward(request, response);
+    }
+
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+
+        String title = request.getParameter("title");
+        String priority = request.getParameter("priority");
+
+        Priority taskPriority = Priority.valueOf(priority);
+        Task newTask = new Task(title, taskPriority);
+
+        boolean created = taskRepository.create(newTask);
+
+        if (created) {
+            response.sendRedirect(request.getContextPath() + "/tasks-list");
+        } else {
+            response.sendRedirect(request.getContextPath() + "/create-task?error=taskExists");
+        }
+    }
+
+}
---
+++ pre-marathone-training/sprint09/src/main/java/servlets/DeleteTaskServlet.java
@@ -0,0 +1,36 @@
+package servlets;
+
+
+import repository.TaskRepo;
+import jakarta.servlet.RequestDispatcher;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+import java.io.IOException;
+
+@WebServlet("/delete-task")
+public class DeleteTaskServlet extends HttpServlet {
+
+    private TaskRepo taskRepository;
+
+    @Override
+    public void init()  {
+        taskRepository = new TaskRepo();
+    }
+
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        int taskId = Integer.parseInt(request.getParameter("id"));
+        if (taskRepository.delete(taskId)) {
+            response.sendRedirect("/tasks-list");
+        } else {
+            request.setAttribute("message", "Task with ID " + taskId + " not found!");
+            request.setAttribute("url", request.getContextPath() + "/delete-task?id=" + taskId);
+            response.setStatus(HttpServletResponse.SC_NOT_FOUND);
+            RequestDispatcher requestDispatcher = request.getRequestDispatcher("WEB-INF/pages/error.jsp");
+            requestDispatcher.forward(request, response);
+        }
+    }
+}
