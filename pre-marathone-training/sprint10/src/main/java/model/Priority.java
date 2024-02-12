a4eb9c0e294803741a5e0ee06907468c7b76d208
Viacheslav Korbut <minowaman777@gmail.com>
2024-02-12 20:13:35 +0200

    Implement all model

---
+++ pre-marathone-training/sprint10/src/main/java/model/Priority.java
@@ -0,0 +1,3 @@
+package model;
+
+public enum Priority {LOW, MEDIUM, HIGH}
--- pre-marathone-training/sprint10/src/main/java/model/Task.java
+++ pre-marathone-training/sprint10/src/main/java/model/Task.java
@@ -1,4 +1,59 @@
 package model;
 
+import java.util.Objects;
+
 public class Task {
+    private int id;
+    private String title;
+    private Priority priority;
+
+    public Task() {
+    }
+
+    public Task(String title, Priority priority) {
+        this.title = title;
+        this.priority = priority;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public Priority getPriority() {
+        return priority;
+    }
+
+    public void setPriority(Priority priority) {
+        this.priority = priority;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Task task = (Task) o;
+        return id == task.id && Objects.equals(title, task.title) && priority == task.priority;
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id, title, priority);
+    }
+
+    @Override
+    public String toString() {
+        return "Task{" +
+                "id=" + id +
+                ", title='" + title + '\'' +
+                ", priority=" + priority +
+                '}';
+    }
 }
