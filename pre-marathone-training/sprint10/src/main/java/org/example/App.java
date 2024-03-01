60ce9d252ec57d3ccc7e31a3db13348e8f98e195
Viacheslav Korbut <minowaman777@gmail.com>
2024-03-01 07:56:19 +0200

    refactor

--- pre-marathone-training/sprint10/src/main/java/org/example/App.java
+++ pre-marathone-training/sprint10/src/main/java/org/example/App.java
@@ -1,29 +1,17 @@
 package org.example;
 
-import model.Priority;
-import model.Task;
-import service.TaskService;
-import service.impl.TaskServiceImpl;
-import service.impl.ToDoServiceImpl;
-import service.impl.UserServiceImpl;
+import org.example.training.MusicPlayer;
+import org.example.training.SpringConfig;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 
 /**
  * @author Viacheslav Korbut
  */
 public class App {
-/*    public static void main(String[] args) {
-*//*        var context = new AnnotationConfigApplicationContext(SpringConfig.class);
+    public static void main(String[] args) {
+        var context = new AnnotationConfigApplicationContext(SpringConfig.class);
         var musicPlayer = context.getBean(MusicPlayer.class);
         musicPlayer.playMusic();
-        context.close();*//*
-        TaskService taskService = new TaskServiceImpl(new ToDoServiceImpl(new UserServiceImpl()));
-        System.out.println(taskService.createTask(new Task("Task #1", Priority.MEDIUM)));
-        System.out.println(taskService.createTask(new Task("Task #3", Priority.MEDIUM)));
-        System.out.println(taskService.getTaskList());
-        System.out.println("Delete: "+taskService.deleteTask("Task #1"));
-        Task ta = new Task("Task #3",Priority.HIGH);
-        System.out.println("Update: "+taskService.updateTask(ta));
-        System.out.println("Read: " + taskService.readTsk("Task #2"));
-
-    }*/
+        context.close();
+    }
 }
