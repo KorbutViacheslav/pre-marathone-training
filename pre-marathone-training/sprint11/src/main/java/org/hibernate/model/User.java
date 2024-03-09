870d97b716e1c4f9e3bbd9f429f0777904fc7d48
Viacheslav Korbut <minowaman777@gmail.com>
2024-03-09 20:15:53 +0200

    as

--- pre-marathone-training/sprint11/src/main/java/org/hibernate/model/User.java
+++ pre-marathone-training/sprint11/src/main/java/org/hibernate/model/User.java
@@ -4,7 +4,9 @@ import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 /**
  * Author: Viacheslav Korbut
@@ -12,6 +14,8 @@ import lombok.Data;
  */
 @Data
 @Entity
+@AllArgsConstructor
+@NoArgsConstructor
 public class User {
     @Id
     @GeneratedValue(strategy = GenerationType.AUTO)
