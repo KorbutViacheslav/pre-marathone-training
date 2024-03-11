467bd01c6052046074ae433bc59e0fe9fa178ae8
Viacheslav Korbut <minowaman777@gmail.com>
2024-03-11 20:39:40 +0200

    a

--- pre-marathone-training/sprint11/src/main/java/org/hibernate/model/Task.java
+++ pre-marathone-training/sprint11/src/main/java/org/hibernate/model/Task.java
@@ -14,6 +14,7 @@ import lombok.NoArgsConstructor;
 @NoArgsConstructor
 @AllArgsConstructor
 @Entity
+@Table(name = "task")
 public class Task {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
