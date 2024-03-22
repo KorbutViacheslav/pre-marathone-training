00956b95ce80524a04d74c9cbad77be9be7ce697
Viacheslav Korbut <minowaman777@gmail.com>
2024-03-22 11:37:24 +0200

    added lazy fetch

--- pre-marathone-training/sprint11/src/main/java/org/hibernate/model/State.java
+++ pre-marathone-training/sprint11/src/main/java/org/hibernate/model/State.java
@@ -25,7 +25,7 @@ public class State {
     @Size(min = 2, max = 300)
     private String name;
 
-    @OneToMany(mappedBy = "state", cascade = CascadeType.ALL)
+    @OneToMany(mappedBy = "state", cascade = CascadeType.ALL,fetch = FetchType.LAZY)
     private List<Task> taskList = new LinkedList<>();
 
     @Override
